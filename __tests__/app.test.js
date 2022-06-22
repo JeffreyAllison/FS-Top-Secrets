a46784eeb175ea75349d734f3d8def61672ccef0
JeffAllison <itsjallison@gmail.com>
2022-06-22 15:13:37 -0700

    added userService import to app.test.js, added signUpAndLogin function and created dodUser object, created first test to create a new user upon sign up

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,14 +2,49 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
+
+const dodUser = {
+  firstName: 'Random',
+  lastName: 'User',
+  email: 'RandomUser@dod.gov',
+  password: '54321',
+};
+
+const signUpAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? dodUser.password;
+
+  const agent = request.agent(app);
+
+  const user = await UserService.create({ ...dodUser, ...userProps });
+
+  const { email } = user;
+  await (
+    await agent.post('/api/v1/users/sessions')
+  ).setEncoding({
+    email,
+    password,
+  });
+  return [agent, user];
+};
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('new user created upon sign up', async () => {
+    const res = await (await request(app).post('/api/v1/users')).send(dodUser);
+    const { firstName, lastName, email } = dodUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      firstName,
+      lastName,
+      email,
+    });
   });
+
   afterAll(() => {
     pool.end();
   });
