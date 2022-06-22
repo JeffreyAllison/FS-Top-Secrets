ec2335a2ba37c9f5ec49bc2d0b0b673570a1deb0
JeffAllison <itsjallison@gmail.com>
2022-06-21 18:08:37 -0700

    added dod users and secrets sql tables

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,23 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP TABLE IF EXISTS dod_users;
+
+DROP TABLE IF EXISTS secrets;
+
+CREATE TABLE dod_users (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  first_name TEXT NOT NULL, 
+  last_name TEXT NOT NULL,
+  email TEXT NOT NULL,
+  password_hash TEXT NOT NULL
+);
+
+CREATE TABLE secrets (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  title TEXT REQUIRED,
+  description TEXT REQUIRED,
+  created_at TIMESTAMP
+);
+
+
